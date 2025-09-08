e9944c2201049760f7b62d26aa1450ff7671bb2b
Minn <minn798088@gmail.com>
2025-09-08 14:53:12 -0400

    update initBad()

--- prog4_sqrt/data.cpp
+++ prog4_sqrt/data.cpp
@@ -14,7 +14,7 @@ void initGood(float *values, int N) {
     for (int i=0; i<N; i++)
     {
         // Todo: Choose values
-        values[i] = 2.998f - i / N;
+        values[i] = 2.999f - i / N;
     }
 }
 
@@ -22,8 +22,13 @@ void initGood(float *values, int N) {
 void initBad(float *values, int N) {
     for (int i=0; i<N; i++)
     {
-        // Todo: Choose values
-        values[i] = 1.0f;
+        // ispc vector work on 8 lanes, make them have highest variance can make them less efficient
+        // This creates greatest load imbalance within SIMD lanes
+        if (i % 8 == 0) {
+            values[i] = 2.999f;
+        } else {
+            values[i] = 1.0f;    // Cheap computation, all wait for the first lane to finish
+        }
     }
 }
 
